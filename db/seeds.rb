118b632677e32930894f0d3078a676ef2d02e36b
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-09-20 20:01:16 +0200

    added product scaffold,seed data, changed some css ,and listed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.delete_all
+Product.create(title: 'Duck',
+  description:
+    %{<p>
+    	<em>Big yellow fluffy duck</em>
+    	</p>},
+    	image_url:'duck.jpg',
+    	price: 26.00)
+
+Product.create(title: 'Kitty',
+  description:
+    %{<p>
+    	<em>Cute little kitty</em>
+    	</p>},
+    	image_url:'kitty.jpg',
+    	price: 56.00)
+
+Product.create(title: 'Puppy',
+  description:
+    %{<p>
+    	<em>Little lost puppy</em>
+    	</p>},
+    	image_url:'puppy.jpg',
+    	price: 56.00)
