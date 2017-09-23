e77960a7175173b898804a050a691e14f5310465
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-09-23 18:22:52 +0200

    fixed test errors by changing image url to real url in fixtures sample data

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,6 +11,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     }
   end
 
+  test "should get index" do
+    get products_url
+    assert_response :success
+  end
+  
   test "should get new" do
     get new_product_url
     assert_response :success
