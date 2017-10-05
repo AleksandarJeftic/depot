4b25c2f7a14ac6c17e5da9091845118f225f47b5
AleksandarJeftic <aleksandarjeftic11@gmail.com>
2017-10-05 11:42:43 +0200

    fixing mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Depot::Application.configure do
+    config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
+
     config.action_mailer.smtp_settings = {
       address:"smtp.gmail.com",
       port:587,
       domain:"domain.of.sender.net",
-      authentication: "login",
+      authentication: "plain",
       user_name:"jeftic66666@gmail.com",
       password:"444saken",
       enable_starttls_auto: true
